8f176d39f59f70ac873c2200d60ef6df03ac81bf
kuldeeparya94 <kuldeeparya3794@gmail.com>
2024-01-28 21:13:26 +0530

    firsts

--- main.py
+++ main.py
@@ -4,18 +4,20 @@ import openai
 # Set your OpenAI API key
 openai.api_key = "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"
 
+
 def chat_gpt(input):
     completion = openai.ChatCompletion.create(
-        model= "gpt-3.5-turbo",
+        model="gpt-3.5-turbo",
         messages=[
             {"role": "user", "content": input},
         ],
-        temperature= 0.2,
-        max_tokens= 150
+        temperature=0.2,
+        max_tokens=150
     )
 
     text = completion.choices[0].message.content
     return text
+
 
 iface = gr.Interface(fn=chat_gpt, inputs="text", outputs="text")
 iface.launch()
